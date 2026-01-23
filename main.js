4ffeb3c50ada6ceea2edd37bbb4ca15e0500d1ac
Solar <abe@solarcosmic.net>
2026-01-23 15:38:22 +1300

    Final (#24)

--- main.js
+++ main.js
@@ -108,7 +108,7 @@ function createMainWindow() {
                 },
             })
             win.setMenu(null);
-            win.webContents.openDevTools();
+            //win.webContents.openDevTools();
             win.setBackgroundColor("#000000");
             win.loadFile("src/index.html");
             /*win.webContents.on("did-finish-load", function() {
